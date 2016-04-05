d39c957aea6ed5c8c8bb93675c4668466ef93ecb
Rick van Osta <osta1995@hotmail.com>
2016-04-05 16:11:45 +0200

    Done button fix

--- www/js/app.js
+++ www/js/app.js
@@ -99,7 +99,7 @@ function onAppReady() {    if( navigator.splashscreen && navigator.splashscreen.
                        
     
     $( "#loginButton" ).on("tap", function() {
-        var ref = window.open('https://voicezone.herokuapp.com/auth/facebook', '_blank', 'location=yes');    
+        var ref = window.open('https://voicezone.herokuapp.com/auth/facebook', '_blank', 'location=yes,closebuttoncaption=Done);    
         
         
         ref.addEventListener('exit', function(){
